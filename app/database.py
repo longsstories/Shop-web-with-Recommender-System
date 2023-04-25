38ded37011d1055353a1bba17448368ca5894d3f
longsstories <longsstories@gmail.com>
2023-04-26 02:08:07 +0700

    update database,update model

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine=create_engine('sqlite:///./datatrain.db'
+engine=create_engine('sqlite:///./datatbase.db'
                      ,connect_args={"check_same_thread":False})
 
 SessionLocal=sessionmaker(bind=engine,autocommit=False,autoflush=False)
