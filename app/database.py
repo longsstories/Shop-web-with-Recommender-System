bb7d13d726c91a58900b0107dd390648bdc8bcd1
longsstories <longsstories@gmail.com>
2023-05-14 00:09:08 +0700

    update product for old user, update databse: table history

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine=create_engine('sqlite:///./database.db'
+engine=create_engine('sqlite:///./database1305.db'
                      ,connect_args={"check_same_thread":False})
 
 SessionLocal=sessionmaker(bind=engine,autocommit=False,autoflush=False)
